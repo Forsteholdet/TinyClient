a50a8ebadac1297e2de498eb3f0947f609f9d1ab
mkakbas <45030188+mkakbas@users.noreply.github.com>
2021-03-12 12:58:24 +0100

    Remove string.h transfer to arduino

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,8 @@
-#include <iostream>
-
-#include <sys/socket.h>
+//#include <iostream>
+//#include <sys/socket.h>
 #include "http_request.h"
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    tinyclient::t_socket socket;
-    http_request client(&socket);
-
-    std::string response = socket.send("bla");
-
-    std::cout << "json response: " << response;
-
     return 0;
 }
