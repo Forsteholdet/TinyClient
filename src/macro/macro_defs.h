ffadde68025b5de284f1a6858797b8e94058feae
Kaare <kaare.hattel@gmail.com>
2021-03-19 14:21:15 +0100

    Extended macros to Win32 and (maybe) ESP32

--- src/macro/macro_defs.h
+++ src/macro/macro_defs.h
@@ -1,10 +1,18 @@
 #include <iostream>
-#if defined __linux
+#if defined (__linux)
     #define PLATFORM "Linux"
+    typedef std::string TCstring;
+#elif defined(_WIN32)
+    #define PLATFORM "Windows"
+    typedef std::string TCstring;
+#elif defined(ESP32)
+    #define PLATFORM "ESP32"
+    include <string>
+    typedef string TCstring;
 #else
 	#define PLATFORM "Others"
 #endif
 
 void detect_OS(){
-    std::cout << "OS is: " << PLATFORM;
+    std::cout << "\nOS is: " << PLATFORM;
 }
