2cefc550898c70d54b79ae67424c1f830ecdf41c
Kaare <kaare.hattel@gmail.com>
2021-03-23 09:42:15 +0100

    Implementation of TCstringLinux and WIP TCstringESP32

--- src/internals/TCstring.h
+++ src/internals/TCstring.h
@@ -7,35 +7,75 @@
 
 #endif //TINY_CLIENT_TCSTRING_H
 
-#if define(__linux)
-#include "string.h"
+#if defined(__linux) || defined(_WIN32)
+
+#include "string"
+
+class TCstringLinux{
+public:
+
+    std::string str;
+
+    TCstringLinux(const char* string) {
+
+        str = std::string(string);
+    }
+
+    int length(){
+        return str.length();
+    }
+
+    TCstringLinux substr(int pos, int count) {
+        auto stringToReturn = str.substr(pos, count);
+        return TCstringLinux(stringToReturn.c_str());
+    }
+
+    bool empty(){
+        return str.empty();
+    }
+
+    friend bool operator== (const TCstringLinux &lhs,const TCstringLinux &rhs){
+        return lhs.str == rhs.str;
+    }
+
+};
+
 #endif
 
 
-class TCstring(){
-public:
-    void strcpy(Tcstring dest, TCstring src){}
+#if defined(ESP32)
 
-    int strlen(const Tcstring *str){}
+#include <Arduino.h>
 
-    TCstring substr(int pos, int count){}
-}
-
-class TCstringLinux(): public TCstring{
+class TCstringEsp32{
 public:
 
-    void strcpy(Tcstring *dest, const Tcstring *src){
-        std::strcpy(dest, src);
+    String str;
 
+    TCstringEsp32(const char* string) {
+
+        str = String(string);
     }
 
-    std::size_t strlen(const Tcstring *str){
-        return std:strlen(str);
+    //void strcpy(TCstring *dest, const TCstring *src){}
+
+    int length(){
+        return str.Length();
     }
 
-    Tcstring substr(int pos, int count) {
-        return std::substr(pos, count)
+//    bool empty(){
+//        return str.Empty();
+//    }
+
+    TCstringEsp32 substr(int pos, int count){
+        auto stringToReturn = str.substring(pos, count);
+        return TCstringEsp32(stringToReturn.c_str());
     }
 
+    friend bool operator== (const TCstringEsp32 &lhs,const TCstringEsp32 &rhs){
+        return lhs.str == rhs.str;
+    }
+};
 
-}
+#endif
+
