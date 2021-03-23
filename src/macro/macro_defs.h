1519b18e0150696cc3b3cf032ea8a641147c0166
Kaare <kaare.hattel@gmail.com>
2021-03-23 09:42:36 +0100

    Updated TCstring types to TCstringLinux or TCstringESP32 based on OS.

--- src/macro/macro_defs.h
+++ src/macro/macro_defs.h
@@ -1,14 +1,15 @@
 #include <iostream>
+#include "internals/TCstring.h"
+
 #if defined (__linux)
     #define PLATFORM "Linux"
-    typedef std::string TCstring;
+    typedef TCstringLinux TCstring;
 #elif defined(_WIN32)
     #define PLATFORM "Windows"
-    typedef std::string TCstring;
+    typedef TCstringLinux TCstring;
 #elif defined(ESP32)
     #define PLATFORM "ESP32"
-    include <string>
-    typedef string TCstring;
+    typedef TCstringEsp32 TCstring;
 #else
 	#define PLATFORM "Others"
 #endif
