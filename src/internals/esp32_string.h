ce527ea16ecb9153b4b8ba28b216b13d2974f533
Kaare <kaare.hattel@gmail.com>
2021-03-30 10:26:27 +0200

    WIP esp32_string.h

--- src/internals/esp32_string.h
+++ src/internals/esp32_string.h
@@ -1,38 +1,82 @@
 #ifndef TINY_CLIENT_ESP32STRING_H
 #define TINY_CLIENT_ESP32STRING_H
 
-#endif //TINY_CLIENT_ESP32STRING_H
+
 #if defined(ESP32)
 #include <Arduino.h>
+#include <String.h>
 
 class ESP32String{
 public:
 
-    String str;
 
-    ESP32String(const char* string) {
 
-        str = String(string);
+    ESP32String(const char* str) {
+
+        _str = String(str);
     }
 
-    //void strcpy(TCstring *dest, const TCstring *src){}
-
-    int length(){
-        return str.Length();
+    ESP32String(String basicString){
+        _str = basicString;
     }
 
-//    bool empty(){
-//        return str.Empty();
-//    }
+    int length() const{
+        return _str.length();
+    }
 
-    ESP32String substr(int pos, int count){
-        auto stringToReturn = str.substring(pos, count);
+    bool empty(){
+        return _str == "";
+    }
+
+    const char* toCharArray() const{
+        return _str.c_str();
+    }
+
+    int find(const ESP32String& str) const{
+        return _str.indexOf(str.toCharArray());
+    }
+
+    int find(const char* c) const{
+        return _str.indexOf(c);
+    }
+
+    bool ends_with(ESP32String const &b) {
+        auto len = b.length();
+        auto pos = _str.length() - len;
+        auto a_arr = _str.c_str();
+        auto b_arr = b._str.c_str();
+        if (pos < 0)
+            return false;
+        auto pos_a = &a_arr[pos];
+        auto pos_b = &b_arr[0];
+        while (*pos_a)
+            if (*pos_a++ != *pos_b++)
+                return false;
+        return true;
+    }
+
+    ESP32String substr(int pos, int count) const{
+        auto stringToReturn = _str.substring(pos, count);
         return ESP32String(stringToReturn.c_str());
     }
 
     friend bool operator== (const ESP32String &lhs, const ESP32String &rhs){
-        return lhs.str == rhs.str;
+        return lhs._str == rhs._str;
     }
+
+    friend ESP32String operator+ (const ESP32String &lhs, const ESP32String &rhs){
+        return lhs._str + rhs._str;
+    }
+
+
+    ESP32String& operator+=(const ESP32String& rhs){
+        this->_str += rhs._str;
+        return *this;
+    }
+
+private:
+    String _str;
 };
 
 #endif
+#endif //TINY_CLIENT_ESP32STRING_H
