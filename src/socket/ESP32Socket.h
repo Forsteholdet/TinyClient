25e77b256599e10343f59b8d5c9ded813026ec90
Anders Aaen Springborg <aaspringborg@gmail.com>
2021-04-14 15:16:43 +0200

    remove namespace

--- src/socket/ESP32Socket.h
+++ src/socket/ESP32Socket.h
@@ -1,12 +1,11 @@
 #ifndef TINY_CLIENT_ESP32SOCKET_H
 #define TINY_CLIENT_ESP32SOCKET_H
 #include "t_socket.h"
+
 using namespace tinyclient;
-{
-    class ESP32Socket : public t_socket {
+class ESP32Socket : public t_socket {
         public:
             bool connect() { return true; };
         private:
     };
-} // namespace tinyclinet
 #endif // TINY_CLIENT_ESP32SOCKET_H
